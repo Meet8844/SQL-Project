ac8734c1435707238bad5349923a39a72eebc68d
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:33:46 +0000

    Having

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -450,3 +450,34 @@ WHERE
     salary > '80000'
 GROUP BY salary
 ORDER BY salary;
+
+
+-- HAVING
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date >= '2000-01-01';
+
+SELECT 
+    *
+FROM
+    employees
+HAVING hire_date >= '2000-01-01';
+
+SELECT 
+    first_name, COUNT(first_name) AS name_count
+FROM
+    employees
+GROUP BY first_name
+HAVING COUNT(first_name) > 250
+ORDER BY first_name;
+
+SELECT 
+    emp_no, AVG(salary)
+FROM
+    salaries
+GROUP BY emp_no
+HAVING AVG(salary) > '120000'
+ORDER BY emp_no;
