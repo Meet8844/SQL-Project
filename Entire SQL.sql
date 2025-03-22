ee85892248c3c8228001342c4a401fd16be4402d
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:17:13 +0000

    Add files via upload
    Create Database

---
+++ Entire SQL.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+SELECT 
+    first_name, last_name
+FROM
+    employees;
+    
+SELECT 
+    *
+FROM
+    employees;
+
+SELECT 
+    *
+FROM
+    departments;
+
+SELECT 
+    dept_no
+FROM
+    departments;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis';
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis';
