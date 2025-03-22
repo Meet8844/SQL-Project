380df32517fb39610383228db0f59c72352f021e
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:39:47 +0000

    Updates

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -634,3 +634,28 @@ SELECT
     *
 FROM
     departments;
+    
+    
+-- UPDATES
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no = 999901;
+
+UPDATE employees 
+SET 
+    first_name = 'Stella',
+    last_name = 'Parkinson',
+    birth_date = '1990-12-31',
+    gender = 'F'
+WHERE
+    emp_no = '999909';
+    
+SELECT 
+    *
+FROM
+    employees
+ORDER BY emp_no DESC
+LIMIT 10;
