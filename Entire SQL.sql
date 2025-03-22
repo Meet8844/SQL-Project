af048174c852b2f0a244f506a12ebe5afb572f97
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:21:44 +0000

    IN and NOT IN

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -107,3 +107,32 @@ WHERE
     gender = 'F'
         AND (first_name = 'Kellie'
         OR first_name = 'Aruna');
+        
+        -- IN & NOT IN
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IN ('Cathie' , 'Mark', 'Nathan');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT IN ('Cathie' , 'Mark', 'Nathan');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IN ('Denis' , 'Elvis');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT IN ('John' , 'Mark', 'Jacob');
