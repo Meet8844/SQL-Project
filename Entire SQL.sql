bc02921bf51df2f0492277caa578e5f9ffdd5484
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:36:27 +0000

    LIMIT

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -503,3 +503,36 @@ WHERE
 GROUP BY emp_no
 HAVING COUNT(from_date) > 1
 ORDER BY emp_no;
+
+
+-- LIMIT
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC
+LIMIT 10;
+
+SELECT 
+    first_name, COUNT(first_name) AS name_count
+FROM
+    employees
+WHERE
+    hire_date > '1999-01-01'
+GROUP BY first_name
+HAVING COUNT(first_name) < 200
+ORDER BY first_name DESC
+LIMIT 100;
+
+SELECT 
+    *
+FROM
+    dept_emp
+LIMIT 100;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY emp_no DESC
+LIMIT 10;
