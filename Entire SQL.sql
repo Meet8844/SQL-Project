59b5454a097a75872f829e07b30fd7dd6278bba6
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:34:56 +0000

    WHERE vs HAVING

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -481,3 +481,25 @@ FROM
 GROUP BY emp_no
 HAVING AVG(salary) > '120000'
 ORDER BY emp_no;
+
+
+-- WHERE vs HAVING
+SELECT 
+    first_name, COUNT(first_name) AS name_count
+FROM
+    employees
+WHERE
+    hire_date > '1999-01-01'
+GROUP BY first_name
+HAVING COUNT(first_name) < 200
+ORDER BY first_name DESC;
+
+SELECT 
+    emp_no
+FROM
+    dept_emp
+WHERE
+    from_date > '2000-01-01'
+GROUP BY emp_no
+HAVING COUNT(from_date) > 1
+ORDER BY emp_no;
