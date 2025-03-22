88eb9aa0c0ad97f070a73819ccc0ff689a542378
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:42:16 +0000

    DELETE Statement

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -698,3 +698,27 @@ SELECT
 FROM
     departments
 ORDER BY dept_no DESC;
+
+
+-- DELETE statements
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no = 999903;
+
+DELETE FROM employees 
+WHERE
+    emp_no = 999903;
+
+ROLLBACK;
+
+SELECT 
+    *
+FROM
+    departments;
+
+DELETE FROM departments 
+WHERE
+    dept_no = 'd010';
