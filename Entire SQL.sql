b5acec0268c9e3dd78444bc13559fd104bdad527
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:25:55 +0000

    IS NULL & IS NOT NULL

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -187,3 +187,69 @@ FROM
     employees
 WHERE
     first_name NOT LIKE ('%Jack%');
+    
+    
+-- BETWEEN... AND... & NOT BETWEEN... AND...
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date NOT BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT 
+    *
+FROM
+    salaries;
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN '66000' AND '70000';
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no NOT BETWEEN '10004' AND '10012';
+
+SELECT 
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no BETWEEN 'd003' AND 'd006';
+
+
+--  IS NULL & IS NOT NULL
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IS NULL;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IS NOT NULL;
+
+SELECT 
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no IS NOT NULL;
