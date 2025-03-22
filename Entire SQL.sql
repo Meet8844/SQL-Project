7b9325fe3c22d0ca8dde9fc57894fb097449c360
Meet Lathiya <meetlathiya301@gmail.com>
2025-03-22 09:31:01 +0000

    Order by

--- Entire SQL.sql
+++ Entire SQL.sql
@@ -365,3 +365,46 @@ SELECT
     COUNT(*)
 FROM
     dept_manager;
+    
+
+-- Order By
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name ASC;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name DESC;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY emp_no DESC;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name , last_name ASC;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY hire_date DESC;
+
+SELECT 
+    first_name
+FROM
+    employees;
